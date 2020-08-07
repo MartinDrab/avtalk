2a44a78c86238f5d470cf158609339be7769b9de
Martin Drab <martin.drab@email.cz>
2020-08-07 23:11:35 +0100

    [MFBroker]: Define and use some callbacks

--- include/mfbroker.h
+++ include/mfbroker.h
@@ -14,6 +14,8 @@
 
 typedef struct _MF_LISTED_MESSAGE {
 	MF_LIST_ENTRY Entry;
+	uint32_t OriginalFlags;
+	uint32_t Padding;
 	MF_MESSAGE_HEADER Header;
 	// Data
 } MF_LISTED_MESSAGE, *PMF_LISTED_MESSAGE;
@@ -27,9 +29,26 @@ typedef struct _MF_CONNECTION {
 	void *Context;
 	struct _MF_BROKER* Broker;
 	MF_LIST_ENTRY MessagesToSend;
-	MF_LIST_ENTRY MessagesReceived;
 } MF_CONNECTION, *PMF_CONNECTION;
 
+typedef enum _EBrokerErrorType {
+	betMessageFailedReceive,
+	betMessageFailedSend,
+	betMessageInvalidSignature,
+	betMessageFailedDecryption,
+	betFailedPoll,
+	betSocketError,
+} EBrokerErrorType, *PEBrokerErrorType;
+
+typedef enum _EBrokerMessageEventType {
+	bmetReady,
+	bmetAboutToEncrypt,
+	bmetAboutToSign,
+	bmetSent,
+} EBrokerMessageEventType, *PEBrokerMessageEventType;
+
+typedef int (MF_BROKER_ERROR_CALLBACK)(EBrokerErrorType Type, int Code, void *Data, void *Context);
+typedef int (MF_BROKER_MESSAGE_CALLBACK)(EBrokerMessageEventType Type, const MF_MESSAGE_HEADER* Header, const void* Data, size_t DataLength, uint32_t OriginalFlags, void* Context);;
 
 typedef struct _MF_BROKER {
 	MFCRYPTO_PUBLIC_KEY PublicKey;
@@ -38,6 +57,12 @@ typedef struct _MF_BROKER {
 	PMF_CONNECTION Connections;
 	int ConnectionCount;
 	volatile BOOL Terminated;
+	HANDLE ThreadHandle;
+	DWORD ThreadId;
+	MF_BROKER_ERROR_CALLBACK *ErrorCallback;
+	void* ErrorCallbackContext;
+	MF_BROKER_MESSAGE_CALLBACK *MessageCallback;
+	void* MessageCallbackContext;
 } MF_BROKER, *PMF_BROKER;
 
 
--- mfbroker/mfbroker.cpp
+++ mfbroker/mfbroker.cpp
@@ -93,8 +93,11 @@ static DWORD WINAPI _MFBrokerThread(PVOID Context)
 	int fdCount = 0;
 	struct pollfd *tmp = NULL;
 	PMF_LISTED_MESSAGE msg = NULL;
+	PMF_LISTED_MESSAGE old = NULL;
 	PMF_CONNECTION conn = NULL;
+	MF_LIST_ENTRY recvList;
 
+	MFList_Init(&recvList);
 	while (!broker->Terminated) {
 		if (fdCount != broker->ConnectionCount) {
 			if (fds != NULL)
@@ -125,8 +128,9 @@ static DWORD WINAPI _MFBrokerThread(PVOID Context)
 		if (waitRes > 0) {
 			tmp = fds;
 			conn = broker->Connections;
-			for (int i = 0; i < fdCount; ++i) {
+			while (conn - broker->Connections < fdCount) {
 				if (tmp->revents & POLLERR) {
+					broker->ErrorCallback(betSocketError, ret, conn, broker->ErrorCallbackContext);
 					++tmp;
 					++conn;
 					continue;
@@ -135,14 +139,26 @@ static DWORD WINAPI _MFBrokerThread(PVOID Context)
 				if (tmp->revents & POLLIN) {
 					ret = _MFMessage_Recv(conn->Socket, &msg);
 					if (ret == 0) {
-						if (msg->Header.Flags & MF_MESSAGE_SIGNED)
+						msg->OriginalFlags = msg->Header.Flags;
+						if (msg->Header.Flags & MF_MESSAGE_SIGNED) {
 							ret = MFMessage_Verify(&msg->Header, &conn->Key);
+							if (ret != 0)
+								broker->ErrorCallback(betMessageInvalidSignature, ret, msg, broker->ErrorCallbackContext);
+						}
 
-						if (msg->Header.Flags & MF_MESSAGE_HEADER_ENCRYPTED)
+						if (ret == 0 && msg->Header.Flags & MF_MESSAGE_HEADER_ENCRYPTED) {
 							ret = MFMessage_HeaderDecrypt(&msg->Header, &broker->PublicKey, &broker->SecretKey);
+							if (ret != 0)
+								broker->ErrorCallback(betMessageFailedDecryption, ret, msg, broker->ErrorCallbackContext);
+						}
 
-						MFList_InsertTail(&conn->MessagesReceived, &msg->Entry);
-					}
+						if (ret == 0) {
+							MFList_InsertTail(&recvList, &msg->Entry);
+							MFGen_RefMemAddRef(msg);
+						}
+
+						MFGen_RefMemRelease(msg);
+					} else broker->ErrorCallback(betMessageFailedReceive, ret, conn, broker->ErrorCallbackContext);
 				}
 
 				if (tmp->revents & POLLHUP) {
@@ -155,13 +171,17 @@ static DWORD WINAPI _MFBrokerThread(PVOID Context)
 				if (tmp->revents & POLLOUT) {
 					while (ret == 0 && !MFList_Empty(&conn->MessagesToSend)) {
 						msg = CONTAINING_RECORD(conn->MessagesToSend.Next, MF_LISTED_MESSAGE, Entry);
-						MFMessage_HeaderEncrypt(&msg->Header, &conn->Key);
-						MFMessage_Sign(&msg->Header, &broker->SecretKey);
+						if (broker->MessageCallback(bmetAboutToEncrypt, &msg->Header, &msg->Header + 1, msg->Header.DataSize, msg->OriginalFlags, broker->MessageCallbackContext))
+							MFMessage_HeaderEncrypt(&msg->Header, &conn->Key);
+
+						if (broker->MessageCallback(bmetAboutToSign, &msg->Header, &msg->Header + 1, msg->Header.MessageSize - sizeof(msg->Header), msg->OriginalFlags, broker->MessageCallbackContext))
+							MFMessage_Sign(&msg->Header, &broker->SecretKey);
+						
 						ret = _MFMessage_Send(conn->Socket, msg);
 						if (ret == 0) {
 							MFList_Remove(&msg->Entry);
 							MFGen_RefMemRelease(msg);
-						}
+						} else broker->ErrorCallback(betMessageFailedSend, ret, conn, broker->ErrorCallbackContext);
 					}
 
 					++tmp;
@@ -175,6 +195,15 @@ static DWORD WINAPI _MFBrokerThread(PVOID Context)
 		} else if (waitRes == SOCKET_ERROR) {
 			ret = WSAGetLastError();
 			broker->Terminated = TRUE;
+			broker->ErrorCallback(betFailedPoll, ret, NULL, broker->ErrorCallbackContext);
+		}
+
+		msg = CONTAINING_RECORD(recvList.Next, MF_LISTED_MESSAGE, Entry);
+		while (&msg->Entry != &recvList) {
+			old = msg;
+			msg = CONTAINING_RECORD(msg->Entry.Next, MF_LISTED_MESSAGE, Entry);
+			broker->MessageCallback(bmetReady, &old->Header, &old->Header + 1, old->Header.DataSize, old->OriginalFlags, broker->MessageCallbackContext);
+			MFGen_RefMemRelease(old);
 		}
 	}
 
